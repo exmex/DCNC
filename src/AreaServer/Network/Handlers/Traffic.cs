475effd409235d7ec842128934b8e95a4e7dfde7
GigaToni <great.mafia2010@gmail.com>
2016-12-10 16:58:09 +0100

    Added command to test traffic packets Added more files for doing research

---
+++ src/AreaServer/Network/Handlers/Traffic.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.Network;
+
+namespace AreaServer.Network.Handlers
+{
+    class Traffic
+    {
+
+        [Packet(Packets.CmdUdpCastTcsSignal)]
+        public static void UdpCastTcsSignal(Packet packet)
+        {
+            /*
+              int m_AreaId;
+              float m_x;
+              float m_y;
+              XiTCSSignal m_signal;
+
+              int m_time;
+              int m_signal;
+              int m_state;
+            */
+
+            packet.Reader.ReadInt32(); // AreaId
+            packet.Reader.ReadSingle(); // X
+            packet.Reader.ReadSingle(); // Y
+
+            packet.Reader.ReadInt32(); // Time
+            packet.Reader.ReadInt32(); // Signal
+            packet.Reader.ReadInt32(); // State
+        }
+
+        [Packet(Packets.CmdUdpCastTcs)]
+        public static void UdpCastTcs(Packet packet)
+        {
+            
+        }
+    }
+}
+
+/*
+  unsigned __int16 m_TrafficCarId;
+  unsigned __int16 m_Owner;
+  unsigned __int16 m_carAttr;
+  unsigned __int16 m_path;
+  XiVec4 m_Pos;
+  XiVec4 m_Vel;
+  int m_OwnTime;
+  int m_dwGlobalTime;
+  int m_FreedTime; 
+*/
--- src/GameServer/Util/GameConsoleCommands.cs
+++ src/GameServer/Util/GameConsoleCommands.cs
@@ -9,17 +9,62 @@ namespace GameServer.Util
     {
         public GameConsoleCommands()
         {
+            Add("traffic", "Arbitrary traffic", (command, args) =>
+            {
+                var packet = new Packet(547);
+                ushort carId = 1;
+                if (args.Count > 1)
+                {
+                    carId = ushort.Parse(args[1]);
+                }
+                packet.Writer.Write(carId); // TCarId
+                packet.Writer.Write((ushort)1); // Owner
+                packet.Writer.Write((ushort)1); // Attr
+                packet.Writer.Write((ushort)0); // Path
+
+                // Pos
+                packet.Writer.Write(1050.522f); // X
+                packet.Writer.Write(-969.07f); // Y
+                packet.Writer.Write(49.054f); // Z
+                packet.Writer.Write(-1.783f); // W
+
+                float x = 0.0f, y = 0.0f;
+                if (args.Count > 2)
+                {
+                    x = float.Parse(args[2]);
+                    y = float.Parse(args[3]);
+                }
+
+                // Velo
+                packet.Writer.Write(x); // X
+                packet.Writer.Write(y); // Y
+                packet.Writer.Write(0.0f); // Z
+                packet.Writer.Write(0.0f); // W
+
+                packet.Writer.Write(0); // owntime
+                packet.Writer.Write(0); // global time
+                packet.Writer.Write(0); // freedtime 
+
+                GameServer.Instance.Server.Broadcast(packet);
+
+                return CommandResult.Okay;
+            });
         }
 
         protected override CommandResult HandleSendPkt(string command, IList<string> args)
         {
             short res;
+            int res2;
             if(args.Count < 2)
                 return CommandResult.Fail;
             if (!short.TryParse(args[1], out res))
                 return CommandResult.InvalidArgument;
 
+            if (!int.TryParse(args[2], out res2) || res2 > 256)
+                return CommandResult.InvalidArgument;
+
             var packet = new Packet(Convert.ToUInt16(args[1]));
+            packet.Writer.Write(new byte[res2]);
             GameServer.Instance.Server.Broadcast(packet);
             return CommandResult.Okay;
         }
