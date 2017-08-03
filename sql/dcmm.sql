b861f780617545946a04f1b32d11ac35ead76835
GigaToni <great.mafia2010@gmail.com>
2017-08-03 12:16:46 +0200

    Fixed SQL Structure mismatch

--- sql/dcmm.sql
+++ sql/dcmm.sql
@@ -1,18 +1,3 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : USBW
-Source Server Version : 50613
-Source Host           : localhost:3306
-Source Database       : dcmm
-
-Target Server Type    : MYSQL
-Target Server Version : 50613
-File Encoding         : 65001
-
-Date: 2017-04-23 00:35:33
-*/
-
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
@@ -27,7 +12,10 @@ CREATE TABLE `characters` (
   `Mito` bigint(20) NOT NULL DEFAULT '1000',
   `Avatar` int(11) NOT NULL DEFAULT '1',
   `Level` int(11) NOT NULL DEFAULT '1',
-  `City` int(11) NOT NULL DEFAULT '1',
+  `BaseExp` int(11) NOT NULL,
+  `CurExp` int(11) NOT NULL,
+  `NextExp` int(11) NOT NULL,
+  `City` int(11) NOT NULL DEFAULT '1' COMMENT '0 = driverDome, 1 = moonpalace',
   `CurrentCarID` int(11) NOT NULL DEFAULT '1',
   `GarageLevel` int(11) NOT NULL DEFAULT '1',
   `TID` bigint(20) NOT NULL DEFAULT '0',
@@ -41,12 +29,68 @@ CREATE TABLE `characters` (
   PRIMARY KEY (`CID`),
   KEY `UID` (`UID`),
   CONSTRAINT `characters_ibfk_1` FOREIGN KEY (`UID`) REFERENCES `users` (`UID`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for friends
+-- ----------------------------
+DROP TABLE IF EXISTS `friends`;
+CREATE TABLE `friends` (
+  `SERVERID` int(18) NOT NULL,
+  `CID` int(18) NOT NULL,
+  `FCID` int(18) NOT NULL,
+  `FSTATE` char(1) DEFAULT 'F',
+  PRIMARY KEY (`SERVERID`,`CID`,`FCID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of friends
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for quests
+-- ----------------------------
+DROP TABLE IF EXISTS `quests`;
+CREATE TABLE `quests` (
+  `ServerId` int(18) NOT NULL,
+  `CID` int(18) NOT NULL,
+  `CNAME` varchar(32) NOT NULL,
+  `QuestId` varchar(20) NOT NULL,
+  `State` int(18) NOT NULL,
+  `FailNum` int(18) NOT NULL,
+  `PlaceIdx` int(18) NOT NULL,
+  `LastDate` int(255) DEFAULT NULL,
+  PRIMARY KEY (`ServerId`,`CID`,`QuestId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for servers
+-- ----------------------------
+DROP TABLE IF EXISTS `servers`;
+CREATE TABLE `servers` (
+  `Id` int(255) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) NOT NULL DEFAULT 'Test',
+  `PlayersOnline` int(5) DEFAULT '0',
+  `MaxPlayers` int(5) DEFAULT '10',
+  `GameServerIp` varchar(255) DEFAULT '127.0.0.1',
+  `GameServerPort` int(5) DEFAULT '11021',
+  `LobbyServerIp` varchar(255) DEFAULT '127.0.0.1',
+  `LobbyServerPort` int(5) DEFAULT '11011',
+  `AreaServer1Ip` varchar(255) DEFAULT '127.0.0.1',
+  `AreaServer1UdpPort` int(5) DEFAULT '10701',
+  `AreaServer1Port` int(5) DEFAULT '11031',
+  `AreaServer2Ip` varchar(255) DEFAULT '127.0.0.1',
+  `AreaServer2UdpPort` int(5) DEFAULT '10702',
+  `AreaServer2Port` int(5) DEFAULT '11041',
+  `RankingServerIp` varchar(255) DEFAULT '127.0.0.1',
+  `RankingServerPort` int(5) DEFAULT '11078',
+  PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
--- Records of characters
+-- Records of servers
 -- ----------------------------
-INSERT INTO `characters` VALUES ('1', '1', 'Administrator', '1480599755', '1000', '1', '99', '1', '1', '1', '0', '15', '1050.522', '-969.070', '49.054', '-1.783', '0', '3');
+INSERT INTO `servers` VALUES ('1', 'Test', '0', '10', '127.0.0.1', '11021', '127.0.0.1', '11011', '127.0.0.1', '10701', '11031', '127.0.0.1', '10702', '11041', '127.0.0.1', '11078');
 
 -- ----------------------------
 -- Table structure for shop
@@ -193,36 +237,26 @@ CREATE TABLE `users` (
   `CreateIP` varchar(15) NOT NULL DEFAULT '127.0.0.1',
   `CreateDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`UID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of users
--- ----------------------------
-INSERT INTO `users` VALUES ('1', 'admin', 'ZMpefYDatvxIP04jXY5CxdhWEiFJHLO8Tee4JfpT+l0=', 'hc8KmszS4WPlCuQwjMPIINwwW9OzRynWdggxBX3Fl6c=', '3113354370', '1', '127.0.0.1', '2016-12-10 22:16:17');
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Table structure for vehicles
 -- ----------------------------
 DROP TABLE IF EXISTS `vehicles`;
 CREATE TABLE `vehicles` (
-  `CID` bigint(20) NOT NULL,
+  `CID` bigint(20) NOT NULL AUTO_INCREMENT,
   `CharID` bigint(20) NOT NULL,
-  `auctionCount` int(11) NOT NULL,
-  `baseColor` int(11) NOT NULL,
-  `carType` int(11) NOT NULL,
-  `grade` int(11) NOT NULL,
-  `mitron` double(11,2) NOT NULL,
-  `kmh` double(11,2) NOT NULL,
-  `slotType` int(11) NOT NULL,
-  `color` int(11) NOT NULL,
-  `mitronCapacity` double(11,2) NOT NULL,
-  `mitronEfficiency` double(11,2) NOT NULL,
+  `auctionCount` int(11) NOT NULL DEFAULT '0',
+  `baseColor` int(11) NOT NULL DEFAULT '0',
+  `carType` int(11) NOT NULL DEFAULT '24',
+  `grade` int(11) NOT NULL DEFAULT '9',
+  `mitron` double(11,2) NOT NULL DEFAULT '0.00',
+  `kmh` double(11,2) NOT NULL DEFAULT '0.00',
+  `slotType` int(11) NOT NULL DEFAULT '0',
+  `color` int(11) NOT NULL DEFAULT '0',
+  `mitronCapacity` double(11,2) NOT NULL DEFAULT '500.00',
+  `mitronEfficiency` double(11,2) NOT NULL DEFAULT '0.00',
   PRIMARY KEY (`CID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
--- ----------------------------
--- Records of vehicles
--- ----------------------------
-INSERT INTO `vehicles` VALUES ('1', '1', '0', '0', '24', '9', '200.00', '220.00', '0', '0', '500.00', '500.00');
-INSERT INTO `vehicles` VALUES ('2', '2', '0', '0', '24', '9', '0.00', '200.00', '0', '0', '0.00', '0.00');
 SET FOREIGN_KEY_CHECKS=1;
