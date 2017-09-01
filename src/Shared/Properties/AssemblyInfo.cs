0779bda65af253d8b467f3b53783d1f273d43494
GigaToni <great.mafia2010@gmail.com>
2017-09-01 13:47:52 +0200

    Version up

--- src/Shared/Properties/AssemblyInfo.cs
+++ src/Shared/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyInformationalVersion("0.1.{dmin:2015}.{chash:6}{!}-{branch}")]
+[assembly: AssemblyInformationalVersion("0.2.{dmin:2015}.{chash:6}{!}-{branch}")]
 /*[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]*/
