9555e694851a30a04bf70fc890cf0c89051119c5
GigaToni <great.mafia2010@gmail.com>
2017-09-04 03:42:00 +0200

    Changed versioning info

--- src/AreaServer/Properties/AssemblyInfo.cs
+++ src/AreaServer/Properties/AssemblyInfo.cs
@@ -6,7 +6,11 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("NeoCity Area Server")]
 [assembly: AssemblyDescription("NeoCity Area Server")]
-[assembly: AssemblyConfiguration("")]
+#if DEBUG
+    [assembly: AssemblyConfiguration("Debug")]
+#else
+    [assembly: AssemblyConfiguration("Release")]
+#endif
 [assembly: AssemblyCompany("DCNC")]
 [assembly: AssemblyProduct("NeoCity Area Server")]
 [assembly: AssemblyCopyright("Copyright © 2016")]
@@ -31,6 +35,12 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyInformationalVersion("0.2.{dmin:2015}.{chash:6}{!}-{branch}")]
+#if DEBUG
+[assembly: AssemblyInformationalVersion("0.2.{dmin:2015}.{chash:6}{!}-{branch}-debug")]
+#else
+[assembly: AssemblyInformationalVersion("0.2.{dmin:2015}.{chash:6}{!}-{branch}-release")]
+#endif
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
 /*[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]*/
--- src/GameServer/Properties/AssemblyInfo.cs
+++ src/GameServer/Properties/AssemblyInfo.cs
@@ -31,6 +31,12 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyInformationalVersion("0.2.{dmin:2015}.{chash:6}{!}-{branch}")]
+#if DEBUG
+[assembly: AssemblyInformationalVersion("0.2.{dmin:2015}.{chash:6}{!}-{branch}-debug")]
+#else
+[assembly: AssemblyInformationalVersion("0.2.{dmin:2015}.{chash:6}{!}-{branch}-release")]
+#endif
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
 /*[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]*/
